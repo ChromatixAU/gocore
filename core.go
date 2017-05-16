fed205d4bee4cbd71168c53aa70b11fa28d45e90
Julian Chan <julian.chan@chromatix.com.au>
2017-05-16 13:59:23 +1000

    test log

--- core.go
+++ core.go
@@ -82,6 +82,7 @@ func NewCore() *Core {
 }
 
 func ( c *Core ) HandleRender() {
+  log.Println( "help" )
   c.Logger.Println( "start handle Render" )
   c.Mux.HandleFunc( "/", func( w http.ResponseWriter, req *http.Request ) {
     c.Logger.Println( "start" )
