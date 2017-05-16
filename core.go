408809b894172d2c81804d6e83e0ae51c5d845bf
Julian Chan <julian.chan@chromatix.com.au>
2017-05-16 14:56:40 +1000

    add redirect fix

--- core.go
+++ core.go
@@ -81,7 +81,7 @@ func ( c *Core ) HandleRender() {
   c.Logger.Println( "start handle Render" )
   c.Mux.HandleFunc( "/", func( w http.ResponseWriter, req *http.Request ) {
     if ( strings.HasSuffix( req.URL.Path, "/" ) ) {
-      http.Redirect(w, req, strings.TrimSuffix( r.URL.Path, "/" ), http.StatusFound )
+      http.Redirect(w, req, strings.TrimSuffix( req.URL.Path, "/" ), http.StatusFound )
       return
     }
     c.Logger.Println( "start" )
